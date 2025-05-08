ae0fa64b324331834d697f0eed6f93d80038c05a
nicki <nickiiitadc4@gmail.com>
2025-05-08 14:37:52 +0200

    He cmabiado otra vez el loading no me convence, haciendo el menu de opciones ....

--- Assets/Scripts/MainMenu/LoadingManager.cs
+++ Assets/Scripts/MainMenu/LoadingManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingManager : MonoBehaviour
 {
+    public Slider progressBar;
+    public string sceneToLoad = "FarmFinal";
+
     void Start()
     {
         StartCoroutine(LoadGameAsync());
@@ -12,16 +16,27 @@ public class LoadingManager : MonoBehaviour
     // declara una corutina , que se usara para cargar la escena en 2do plano sin congelar el juego
     IEnumerator LoadGameAsync()
     {
-        //aqui espera 1 segundo antes de continuar 
-       yield return new WaitForSeconds(1f); 
+
+
         //comineza la carga asincrona , este devuelve un obj de tio AsyncOperation que nos dice cuanto ha cargado la scene
         //y si ya esta terminada op.isDone 
-        AsyncOperation op = SceneManager.LoadSceneAsync(SceneLoader.scene);
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneToLoad);
+        op.allowSceneActivation = false;
 
         while (!op.isDone)//el bucle se repite hasta no haber terminado cargar 
         {
-            yield return null; // aca espera al siguiente frame y vuelve a comprobar
+            float progress = Mathf.Clamp01(op.progress / 0.9f);
+            progressBar.value = progress;
+            if (op.progress >= 0.9f)
+            {
+                {//aqui espera 1 segundo antes de continuar 0    
+                    yield return new WaitForSeconds(0.5f);
+                    op.allowSceneActivation = true;
+                }
+
+                yield return null; // aca espera al siguiente frame y vuelve a comprobar
+            }
         }
+
     }
-  
 }
